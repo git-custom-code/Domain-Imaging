c103864f09cb6aa105ceb6a1a7449690cd732c42
Stefan Loeffler <s.loeffler@custom-co.de>
2018-04-26 16:46:04 +0200

    Initial commit

---
+++ src/Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("Domain.Imaging")]
+
+[assembly: AssemblyCompany("CustomCode")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("C# imaging library")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
