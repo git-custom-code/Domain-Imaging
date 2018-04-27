ca0f887b847187115fb6e64daf6e99995e7ec468
Stefan Loeffler <s.loeffler@custom-co.de>
2018-04-27 19:12:00 +0200

    added unit tests for image memory buffer

---
+++ src/Test.Domain.Imaging/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Domain.Imaging")]
+[assembly: AssemblyDescription("Automated tests for the Domain.Imaging assembly.")]
+[assembly: Guid("9f3716ee-fdba-4540-be99-c7930b057611")]
