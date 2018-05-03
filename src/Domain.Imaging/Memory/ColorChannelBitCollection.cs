4b2cbbcd881c79da082bb675c068eff420d2b529
Stefan Loeffler <s.loeffler@custom-co.de>
2018-05-03 17:02:32 +0200

    moved IImageMemory to Data.Imaging

--- src/Domain.Imaging/Memory/ColorChannelBitCollection.cs
+++ src/Domain.Imaging/Memory/ColorChannelBitCollection.cs
@@ -1,5 +1,6 @@
 namespace CustomCode.Domain.Imaging.Memory
 {
+    using Data.Imaging.Memory;
     using System.Collections.Generic;
 
     /// <summary>
--- src/Domain.Imaging/Properties/AssemblyInfo.cs
+++ src/Domain.Imaging/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Domain.Imaging")]
 [assembly: AssemblyDescription("Imaging library for c#.")]
-[assembly: Guid("9f3716ee-fdba-4540-be99-c7930b057601")]
+[assembly: Guid("9f3716ee-fdba-4540-be99-c7930b057003")]
