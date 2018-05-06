9570b2171fa79917a205afc0bf54bfa000aae66b
Stefan Loeffler <s.loeffler@custom-co.de>
2018-05-06 22:58:41 +0200

    added interfaces for image decorators

---
+++ src/Domain/Imaging.Contracts/IImageDecorator.cs
@@ -0,0 +1,8 @@
+namespace CustomCode.Domain.Imaging
+{
+    /// <summary>
+    /// Marker interface for decorator implementations for a single <see cref="IImage"/>.
+    /// </summary>
+    public interface IImageDecorator
+    { }
+}
