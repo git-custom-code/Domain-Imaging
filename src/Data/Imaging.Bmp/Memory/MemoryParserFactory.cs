c9c47f17ffd48715c41c5c6832d0a13c31baf9fd
Stefan Loeffler <s.loeffler@custom-co.de>
2018-10-16 00:16:08 +0200

    added parser logic for 1bit bitmap pixel data

---
+++ src/Data/Imaging.Bmp/Memory/MemoryParserFactory.cs
@@ -0,0 +1,42 @@
+namespace CustomCode.Data.Imaging.Memory.Bmp
+{
+    using Core.Composition;
+    using Imaging.Bmp;
+    using System;
+
+    /// <summary>
+    /// A factory that can be used to create an <see cref="IMemoryParser"/> that can be
+    /// used to parse a bitmap's raw pixel data and returns a corresponding <see cref="IImageMemory"/>
+    /// representation.
+    /// </summary>
+    [Export(typeof(IMemoryParserFactory))]
+    public sealed class MemoryParserFactory : IMemoryParserFactory
+    {
+        #region Logic
+
+        /// <inheritdoc />
+        public IMemoryParser Create(MemoryAlignment alignment, IColorTable colorTable, InfoHeader header)
+        {
+            if (header.BitsPerPixel == 1)
+            {
+                if (colorTable.IsMonochrome())
+                {
+                    if (colorTable[0].red == 255)
+                    {
+                        return new OneBitWhiteBlackParser(alignment, header.Height, (uint)header.Width);
+                    }
+                    return new OneBitBlackWhiteParser(alignment, header.Height, (uint)header.Width);
+                }
+                else if (colorTable.IsGrayScale())
+                {
+                    return new OneBitGrayScaleParser(alignment, colorTable, header.Height, (uint)header.Width);
+                }
+                return new OneBitRgbParser(alignment, colorTable, header.Height, (uint)header.Width);
+            }
+
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Data/Imaging.Bmp/Memory/OneBit/OneBitWhiteBlackParser.cs
@@ -0,0 +1,93 @@
+namespace CustomCode.Data.Imaging.Memory.Bmp
+{
+    using Memory;
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// A <see cref="IMemoryParser"/> for 1bit bitmaps where zero bits are interpreted as white.
+    /// </summary>
+    public sealed class OneBitWhiteBlackParser : IMemoryParser
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="OneBitWhiteBlackParser"/> type.
+        /// </summary>
+        /// <param name="alignment"> The alignment of the parsed <see cref="IImageMemory"/>. </param>
+        /// <param name="height"> The number of pixels in y-direction of the parsed <see cref="IImageMemory"/>. </param>
+        /// <param name="width"> The number of pixels in x-direction of the parsed <see cref="IImageMemory"/>. </param>
+        public OneBitWhiteBlackParser(MemoryAlignment alignment, int height, uint width)
+        {
+            Alignment = alignment;
+            Height = height;
+            Width = width;
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// Gets the alignment of the parsed <see cref="IImageMemory"/>.
+        /// </summary>
+        private MemoryAlignment Alignment { get; }
+
+        /// <summary>
+        /// Gets the number of pixels in y-direction of the parsed <see cref="IImageMemory"/>.
+        /// </summary>
+        private int Height { get; }
+
+        /// <summary>
+        /// Gets the number of pixels in x-direction of the parsed <see cref="IImageMemory"/>.
+        /// </summary>
+        private uint Width { get; }
+
+        #endregion
+
+        #region Logic
+
+        /// <inheritdoc />
+        public IImageMemory Parse(BinaryReader reader)
+        {
+            var memory = new ImageMemory((Width, (uint)Math.Abs(Height)), Alignment, ColorChannels.Monochrome, MemoryPrecision.OneBit);
+            var bytesPerRow = ((Width + 7u) / 8u);
+            var padding = bytesPerRow % 4;
+            if (padding > 0u)
+            {
+                padding = 4u - bytesPerRow;
+            }
+
+            var data = memory.AsArray();
+            if (Height > 0) // rows are stored bottom up
+            {
+                for (var h = Height - 1; h >= 0; --h)
+                {
+                    var offset = h * memory.SizePerAlignedRow;
+                    for (var w = 0u; w < bytesPerRow; ++w)
+                    {
+                        data[offset + w] = (byte)~reader.ReadByte();
+                    }
+                    reader.BaseStream.Position += padding;
+                }
+            }
+            else // rows are stored top down
+            {
+                var absHeight = (uint)(Height * -1);
+                for (var h = 0u; h < absHeight; ++h)
+                {
+                    var offset = h * memory.SizePerAlignedRow;
+                    for (var w = 0u; w < Width; ++w)
+                    {
+                        data[offset + w] = (byte)~reader.ReadByte();
+                    }
+                    reader.BaseStream.Position += padding;
+                }
+            }
+
+            return memory;
+        }
+        
+        #endregion
+    }
+}
