18cd8e3996ee5b255989173cdf26080770d72cd9
Stefan Loeffler <s.loeffler@custom-co.de>
2018-12-28 16:38:20 +0100

    added tests for bitmaps

--- src/Test/Data.Imaging.Bmp/BitmapParserTests.cs
+++ src/Test/Data.Imaging.Bmp/BitmapParserTests.cs
@@ -1,7 +1,8 @@
-namespace CustomCode.Data.Imaging.Tiff.Tests
+namespace CustomCode.Data.Imaging.Bmp.Tests
 {
     using Bmp;
     using Memory;
+    using Memory.Bmp;
     using System.IO;
     using Test.BehaviorDrivenDevelopment;
     using Xunit;
@@ -12,11 +13,91 @@ namespace CustomCode.Data.Imaging.Tiff.Tests
     [IntegrationTest]
     public class BitmapParserTests : TestCase
     {
+        [Fact(DisplayName = "Parse a monochrome bitmap with 1bit depth and black/white color palette")]
+        public void ParseBitmapMonochromeWithBlackWhitePalette()
+        {
+            Given(() => new BitmapParser(new MemoryParserFactory()))
+            .Also(() => new BinaryReader(File.OpenRead(@".\Data\Valid\pal1.bmp")))
+            .When((parser, reader) => parser.Parse(reader, MemoryAlignment.None))
+            .Then(memory =>
+                {
+                    memory.Size.Should().Be(1024);
+                    memory.SizePerAlignedRow.Should().Be(16);
+                    memory.SizePerChannel.Should().Be(16 * 64);
+                    memory.SizePerPixel.Should().Be(127);
+                    memory.Stride.Should().Be(0);
+                });
+        }
+
+        [Fact(DisplayName = "Parse a monochrome bitmap with 1bit depth and white/black color palette")]
+        public void ParseBitmapMonochromeWithWhiteBlackColorPalette()
+        {
+            Given(() => new BitmapParser(new MemoryParserFactory()))
+            .Also(() => new BinaryReader(File.OpenRead(@".\Data\Valid\pal1wb.bmp")))
+            .When((parser, reader) => parser.Parse(reader, MemoryAlignment.None))
+            .Then(memory =>
+                {
+                    memory.Size.Should().Be(1024);
+                    memory.SizePerAlignedRow.Should().Be(16);
+                    memory.SizePerChannel.Should().Be(16 * 64);
+                    memory.SizePerPixel.Should().Be(127);
+                    memory.Stride.Should().Be(0);
+                });
+        }
+
+        [Fact(DisplayName = "Parse a monochrome bitmap with 1bit depth and rgb color palette")]
+        public void ParseBitmapMonochromeWithRgbColorPalette()
+        {
+            Given(() => new BitmapParser(new MemoryParserFactory()))
+            .Also(() => new BinaryReader(File.OpenRead(@".\Data\Valid\pal1bg.bmp")))
+            .When((parser, reader) => parser.Parse(reader, MemoryAlignment.None))
+            .Then(memory =>
+                {
+                    memory.Size.Should().Be(127 * 64 * 3);
+                    memory.SizePerAlignedRow.Should().Be(127);
+                    memory.SizePerChannel.Should().Be(127 * 64);
+                    memory.SizePerPixel.Should().Be(3);
+                    memory.Stride.Should().Be(0);
+                });
+        }
+
+        [Fact(DisplayName = "Parse a rgb bitmap with 4bit depth")]
+        public void ParseBitmapRgb4()
+        {
+            Given(() => new BitmapParser(new MemoryParserFactory()))
+            .Also(() => new BinaryReader(File.OpenRead(@".\Data\Valid\pal4.bmp")))
+            .When((parser, reader) => parser.Parse(reader, MemoryAlignment.None))
+            .Then(memory =>
+            {
+                memory.Size.Should().Be(127 * 64 * 3);
+                memory.SizePerAlignedRow.Should().Be(127);
+                memory.SizePerChannel.Should().Be(127 * 64);
+                memory.SizePerPixel.Should().Be(3);
+                memory.Stride.Should().Be(0);
+            });
+        }
+
+        [Fact(DisplayName = "Parse a gray scale bitmap with 4bit depth")]
+        public void ParseBitmapGrayScale4()
+        {
+            Given(() => new BitmapParser(new MemoryParserFactory()))
+            .Also(() => new BinaryReader(File.OpenRead(@".\Data\Valid\pal4gs.bmp")))
+            .When((parser, reader) => parser.Parse(reader, MemoryAlignment.None))
+            .Then(memory =>
+            {
+                memory.Size.Should().Be(127 * 64);
+                memory.SizePerAlignedRow.Should().Be(127);
+                memory.SizePerChannel.Should().Be(127 * 64);
+                memory.SizePerPixel.Should().Be(1);
+                memory.Stride.Should().Be(0);
+            });
+        }
+
         [Fact(DisplayName = "Parse a rgb bitmap with 24bit depth")]
         [IntegrationTest]
         public void ParseBitmapRgb24()
         {
-            Given(() => new BitmapParser())
+            Given(() => new BitmapParser(new MemoryParserFactory()))
             .Also(() => new BinaryReader(File.OpenRead(@".\Data\Valid\rgb24.bmp")))
             .When((parser, reader) => parser.Parse(reader, MemoryAlignment.None))
             .Then(memory =>
@@ -27,6 +108,22 @@ namespace CustomCode.Data.Imaging.Tiff.Tests
                     memory.SizePerPixel.Should().Be(3);
                     memory.Stride.Should().Be(0);
                 });
+        }
+
+        [Fact(DisplayName = "Parse a rgb bitmap with 24bit depth and color palette")]
+        public void ParseBitmapRgb24WithColorPalette()
+        {
+            Given(() => new BitmapParser(new MemoryParserFactory()))
+            .Also(() => new BinaryReader(File.OpenRead(@".\Data\Valid\rgb24pal.bmp")))
+            .When((parser, reader) => parser.Parse(reader, MemoryAlignment.None))
+            .Then(memory =>
+            {
+                memory.Size.Should().Be(127 * 64 * 3);
+                memory.SizePerAlignedRow.Should().Be(127);
+                memory.SizePerChannel.Should().Be(127 * 64);
+                memory.SizePerPixel.Should().Be(3);
+                memory.Stride.Should().Be(0);
+            });
         }
     }
 }
