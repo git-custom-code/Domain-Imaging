9dc40fdcdd777c44983efb6c17f78b8b0cb79860
Stefan Loeffler <s.loeffler@custom-co.de>
2019-09-24 17:53:41 +0200

    switched to .Net Core 3.0

--- src/Data/Imaging.Bmp/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using CustomCode.Core.Composition;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Data.Imaging.Bmp")]
-[assembly: AssemblyDescription("Imaging library for c#.")]
-[assembly: Guid("9f3716ee-fdba-4540-be99-c7930b057101")]
-[assembly: IocVisibleAssembly]
