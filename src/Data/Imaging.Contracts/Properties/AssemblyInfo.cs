e20dd046769b7f2411691cfa8b316314cee23e99
Stefan Loeffler <s.loeffler@custom-co.de>
2019-09-24 17:33:26 +0200

    switched to .Net Core 3.0

--- src/Data/Imaging.Contracts/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Data.Imaging.Contracts")]
-[assembly: AssemblyDescription("Imaging library for c#.")]
-[assembly: Guid("9f3716ee-fdba-4540-be99-c7930b057003")]
