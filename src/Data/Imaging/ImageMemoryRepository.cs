df31a6616320331f37138822e4f71e9267a0b3e0
Stefan Loeffler <s.loeffler@custom-co.de>
2022-06-11 16:43:40 +0200

    Upgrade to file scoped namespaces

--- src/Data/Imaging/ImageMemoryRepository.cs
+++ src/Data/Imaging/ImageMemoryRepository.cs
@@ -1,101 +1,100 @@
-namespace CustomCode.Data.Imaging
+namespace CustomCode.Data.Imaging;
+
+using Core.Composition;
+using Memory;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Implementation for a repository to load image files from disk.
+/// </summary>
+[Export(typeof(IImageMemoryRepository))]
+public sealed class ImageMemoryRepository : IImageMemoryRepository
 {
-    using Core.Composition;
-    using Memory;
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Runtime.InteropServices;
-    using System.Threading;
-    using System.Threading.Tasks;
+    #region Dependencies
 
     /// <summary>
-    /// Implementation for a repository to load image files from disk.
+    /// Creates a new instance of the <see cref="ImageMemoryRepository"/> type.
     /// </summary>
-    [Export(typeof(IImageMemoryRepository))]
-    public sealed class ImageMemoryRepository : IImageMemoryRepository
+    /// <param name="imageParsers"> A collection with supported image file parsers. </param>
+    public ImageMemoryRepository(IEnumerable<IImageFileParser> imageParsers)
     {
-        #region Dependencies
-
-        /// <summary>
-        /// Creates a new instance of the <see cref="ImageMemoryRepository"/> type.
-        /// </summary>
-        /// <param name="imageParsers"> A collection with supported image file parsers. </param>
-        public ImageMemoryRepository(IEnumerable<IImageFileParser> imageParsers)
-        {
-            ImageParsers = imageParsers ?? Enumerable.Empty<IImageFileParser>();
-        }
-
-        /// <summary>
-        /// Gets a collection with supported image file parsers.
-        /// </summary>
-        private IEnumerable<IImageFileParser> ImageParsers { get; }
-
-        #endregion
-
-        #region Logic
-
-        /// <inheritdoc />
-        public IImageMemory LoadFrom(string path, MemoryAlignment? alignment = null)
-        {
-            var (parser, memoryAlignment) = ResolveParser(path, alignment);
-
-            using var stream = File.OpenRead(path);
-            using var reader = new BinaryReader(stream);
-
-            return parser.Parse(reader, memoryAlignment);
-        }
-
-        /// <inheritdoc />
-        public async Task<IImageMemory> LoadFromAsync(string path, MemoryAlignment? alignment = null, CancellationToken? token = null)
-        {
-            var (parser, memoryAlignment) = ResolveParser(path, alignment);
-
-            using var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.Asynchronous);
-            using var reader = new BinaryReader(stream);
-
-            var memory = await parser.ParseAsync(reader, memoryAlignment, token);
-            return memory;
-        }
-
-        /// <summary>
-        /// Try to resolve a parser from the collection of registered ones that can be used to load the specified
-        /// image memory from disk.
-        /// </summary>
-        /// <param name="path"> The full path to the image file. </param>
-        /// <param name="alignment"> The desired memory alignment of the loaded <see cref="IImageMemory"/>. </param>
-        /// <returns> The resolved parser and memory alignment. </returns>
-        private (IImageFileParser parser, MemoryAlignment memoryAlignment) ResolveParser(string path, MemoryAlignment? alignment)
-        {
-            if (!File.Exists(path))
-            {
-                throw new FileNotFoundException("Image file not found", path);
-            }
-
-            var imageExtension = Path.GetExtension(path);
-            var parser = ImageParsers.FirstOrDefault(p => p.CanParse(imageExtension));
-            if (parser == null)
-            {
-                throw new NotSupportedException($"Image file format {imageExtension} not supported");
-            }
-
-            if (!alignment.HasValue)
-            {
-                if (RuntimeInformation.OSArchitecture == Architecture.X64 ||
-                    RuntimeInformation.OSArchitecture == Architecture.Arm64)
-                {
-                    return (parser, MemoryAlignment.At64Bit);
-                }
-                else
-                {
-                    return (parser, MemoryAlignment.At32Bit);
-                }
-            }
-
-            return (parser, alignment.Value);
-        }
-
-        #endregion
+        ImageParsers = imageParsers ?? Enumerable.Empty<IImageFileParser>();
     }
+
+    /// <summary>
+    /// Gets a collection with supported image file parsers.
+    /// </summary>
+    private IEnumerable<IImageFileParser> ImageParsers { get; }
+
+    #endregion
+
+    #region Logic
+
+    /// <inheritdoc cref="IImageMemoryRepository" />
+    public IImageMemory LoadFrom(string path, MemoryAlignment? alignment = null)
+    {
+        var (parser, memoryAlignment) = ResolveParser(path, alignment);
+
+        using var stream = File.OpenRead(path);
+        using var reader = new BinaryReader(stream);
+
+        return parser.Parse(reader, memoryAlignment);
+    }
+
+    /// <inheritdoc cref="IImageMemoryRepository" />
+    public async Task<IImageMemory> LoadFromAsync(string path, MemoryAlignment? alignment = null, CancellationToken? token = null)
+    {
+        var (parser, memoryAlignment) = ResolveParser(path, alignment);
+
+        using var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.Asynchronous);
+        using var reader = new BinaryReader(stream);
+
+        var memory = await parser.ParseAsync(reader, memoryAlignment, token);
+        return memory;
+    }
+
+    /// <summary>
+    /// Try to resolve a parser from the collection of registered ones that can be used to load the specified
+    /// image memory from disk.
+    /// </summary>
+    /// <param name="path"> The full path to the image file. </param>
+    /// <param name="alignment"> The desired memory alignment of the loaded <see cref="IImageMemory"/>. </param>
+    /// <returns> The resolved parser and memory alignment. </returns>
+    private (IImageFileParser parser, MemoryAlignment memoryAlignment) ResolveParser(string path, MemoryAlignment? alignment)
+    {
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException("Image file not found", path);
+        }
+
+        var imageExtension = Path.GetExtension(path);
+        var parser = ImageParsers.FirstOrDefault(p => p.CanParse(imageExtension));
+        if (parser == null)
+        {
+            throw new NotSupportedException($"Image file format {imageExtension} not supported");
+        }
+
+        if (!alignment.HasValue)
+        {
+            if (RuntimeInformation.OSArchitecture == Architecture.X64 ||
+                RuntimeInformation.OSArchitecture == Architecture.Arm64)
+            {
+                return (parser, MemoryAlignment.At64Bit);
+            }
+            else
+            {
+                return (parser, MemoryAlignment.At32Bit);
+            }
+        }
+
+        return (parser, alignment.Value);
+    }
+
+    #endregion
 }
--- src/Data/Imaging/Memory/ImageMemory.cs
+++ src/Data/Imaging/Memory/ImageMemory.cs
@@ -1,171 +1,170 @@
-namespace CustomCode.Data.Imaging.Memory
+namespace CustomCode.Data.Imaging.Memory;
+
+using System;
+
+/// <summary>
+/// Abstraction for a <see cref="byte"/> array that holds an image's pixel data.
+/// </summary>
+/// <remarks>
+/// The image memory supports the following:
+/// - data precision can be stored as 1bit, 8bit or 16bit per color channel per pixel
+/// - data can be stored with 1 - 4 color channels (monochrome, gray, gray alpha, rgb or rgba) per pixel
+/// - data can be aligned at none, 16bit or 32bit boundaries to benefit from memory burst read mode.
+/// </remarks>
+public sealed class ImageMemory : IImageMemory
 {
-    using System;
+    #region Dependencies
 
     /// <summary>
-    /// Abstraction for a <see cref="byte"/> array that holds an image's pixel data.
+    /// Creates a new instance of the <see cref="ImageMemory"/> type.
     /// </summary>
-    /// <remarks>
-    /// The image memory supports the following:
-    /// - data precision can be stored as 1bit, 8bit or 16bit per color channel per pixel
-    /// - data can be stored with 1 - 4 color channels (monochrome, gray, gray alpha, rgb or rgba) per pixel
-    /// - data can be aligned at none, 16bit or 32bit boundaries to benefit from memory burst read mode.
-    /// </remarks>
-    public sealed class ImageMemory : IImageMemory
+    /// <param name="dimension"> The memory's dimensions (width and height). </param>
+    /// <param name="alignment"> The memory's alignment (none, 32bit or 64bit). </param>
+    /// <param name="colorChannels"> The memory's number of color channels (Monochrome, Grey, GreyAlpha, Rgb or Rgba). </param>
+    /// <param name="precision"> The memory's precision per color channel per pixel (1bit, 8bit or 16bit). </param>
+    public ImageMemory(
+        (uint width, uint height) dimension,
+        MemoryAlignment alignment,
+        ColorChannels colorChannels,
+        MemoryPrecision precision)
     {
-        #region Dependencies
+        Alignment = alignment;
+        ColorChannels = colorChannels;
+        Precision = precision;
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="ImageMemory"/> type.
-        /// </summary>
-        /// <param name="dimension"> The memory's dimensions (width and height). </param>
-        /// <param name="alignment"> The memory's alignment (none, 32bit or 64bit). </param>
-        /// <param name="colorChannels"> The memory's number of color channels (Monochrome, Grey, GreyAlpha, Rgb or Rgba). </param>
-        /// <param name="precision"> The memory's precision per color channel per pixel (1bit, 8bit or 16bit). </param>
-        public ImageMemory(
-            (uint width, uint height) dimension,
-            MemoryAlignment alignment,
-            ColorChannels colorChannels,
-            MemoryPrecision precision)
+        SizePerPixel = (uint)((byte)ColorChannels * (byte)precision);
+        if (SizePerPixel == 0)
         {
-            Alignment = alignment;
-            ColorChannels = colorChannels;
-            Precision = precision;
-
-            SizePerPixel = (uint)((byte)ColorChannels * (byte)precision);
-            if (SizePerPixel == 0)
+            ColorChannels = ColorChannels.Monochrome;
+            Precision = MemoryPrecision.OneBit;
+            SizePerPixel = dimension.width;
+            SizePerUnalignedRow = (uint)(dimension.width / 8.0);
+            if (dimension.width % 8 != 0)
             {
-                ColorChannels = ColorChannels.Monochrome;
-                Precision = MemoryPrecision.OneBit;
-                SizePerPixel = dimension.width;
-                SizePerUnalignedRow = (uint)(dimension.width / 8.0);
-                if (dimension.width % 8 != 0)
-                {
-                    ++SizePerUnalignedRow;
-                }
+                ++SizePerUnalignedRow;
             }
-            else
-            {
-                SizePerUnalignedRow = dimension.width * (uint)Precision;
-            }
-
-            Stride = 0;
-            var inverseStride = SizePerUnalignedRow % (uint)Alignment;
-            if (inverseStride > 0)
-            {
-                Stride = (byte)((uint)Alignment - inverseStride);
-            }
-
-            SizePerAlignedRow = SizePerUnalignedRow + Stride;
-            SizePerChannel = SizePerAlignedRow * dimension.height;
-
-            var dataCount = SizePerChannel;
-            if (ColorChannels != ColorChannels.Monochrome)
-            {
-                dataCount = (uint)ColorChannels * SizePerChannel;
-            }
-            Data = new byte[dataCount];
         }
-
-        #endregion
-
-        #region Data
-
-        /// <inheritdoc />
-        public MemoryAlignment Alignment { get; }
-
-        /// <inheritdoc />
-        public ColorChannels ColorChannels { get; }
-
-        /// <summary>
-        /// Get the memory's raw data as byte array.
-        /// </summary>
-        private byte[] Data { get; }
-
-        /// <inheritdoc />
-        public MemoryPrecision Precision { get; }
-
-        /// <inheritdoc />
-        public uint Size
+        else
         {
-            get { return (uint)Data.Length; }
+            SizePerUnalignedRow = dimension.width * (uint)Precision;
         }
 
-        /// <inheritdoc />
-        public uint SizePerAlignedRow { get; }
-
-        /// <inheritdoc />
-        public uint SizePerChannel { get; }
-
-        /// <inheritdoc />
-        public uint SizePerPixel { get; }
-
-        /// <summary>
-        /// Gets the number of bytes per color channel per image row.
-        /// </summary>
-        private uint SizePerUnalignedRow { get; }
-
-        /// <inheritdoc />
-        public byte Stride { get; }
-
-        #endregion
-
-        #region Logic
-
-        /// <inheritdoc />
-        public byte[] AsArray()
+        Stride = 0;
+        var inverseStride = SizePerUnalignedRow % (uint)Alignment;
+        if (inverseStride > 0)
         {
-            return Data;
+            Stride = (byte)((uint)Alignment - inverseStride);
         }
 
-        /// <inheritdoc />
-        public Memory<byte> AsMemory()
+        SizePerAlignedRow = SizePerUnalignedRow + Stride;
+        SizePerChannel = SizePerAlignedRow * dimension.height;
+
+        var dataCount = SizePerChannel;
+        if (ColorChannels != ColorChannels.Monochrome)
         {
-            return new Memory<byte>(Data);
+            dataCount = (uint)ColorChannels * SizePerChannel;
         }
-
-        /// <inheritdoc />
-        public ReadOnlyMemory<byte> AsReadOnlyMemory()
-        {
-            return new ReadOnlyMemory<byte>(Data);
-        }
-
-        /// <inheritdoc />
-        public ReadOnlySpan<byte> AsReadOnlySpan()
-        {
-            return new ReadOnlySpan<byte>(Data);
-        }
-
-        /// <inheritdoc />
-        public Span<byte> AsSpan()
-        {
-            return new Span<byte>(Data);
-        }
-
-        /// <inheritdoc />
-        public override string ToString()
-        {
-            var length = Data.Length;
-            var unit = "byte";
-            if (Data.Length > 1024 && Data.Length < 1024 * 1024)
-            {
-                length /= 1024;
-                unit = "kb";
-            }
-            else if (Data.Length > 1024 * 1024 && Data.Length < 1024 * 1024 * 1024)
-            {
-                length /= 1024 * 1024;
-                unit = "mb";
-            }
-            else if (Data.Length > 1024 * 1024 * 1024)
-            {
-                length /= 1024 * 1024 * 1024;
-                unit = "gb";
-            }
-
-            return $"Alignment: {Alignment}, Color Channels: {ColorChannels}, Precision: {Precision}, Size: {length}{unit}";
-        }
-
-        #endregion
+        Data = new byte[dataCount];
     }
+
+    #endregion
+
+    #region Data
+
+    /// <inheritdoc cref="IImageMemory" />
+    public MemoryAlignment Alignment { get; }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public ColorChannels ColorChannels { get; }
+
+    /// <summary>
+    /// Gets the memory's raw data as byte array.
+    /// </summary>
+    private byte[] Data { get; }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public MemoryPrecision Precision { get; }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public uint Size
+    {
+        get { return (uint)Data.Length; }
+    }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public uint SizePerAlignedRow { get; }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public uint SizePerChannel { get; }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public uint SizePerPixel { get; }
+
+    /// <summary>
+    /// Gets the number of bytes per color channel per image row.
+    /// </summary>
+    private uint SizePerUnalignedRow { get; }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public byte Stride { get; }
+
+    #endregion
+
+    #region Logic
+
+    /// <inheritdoc cref="IImageMemory" />
+    public byte[] AsArray()
+    {
+        return Data;
+    }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public Memory<byte> AsMemory()
+    {
+        return new Memory<byte>(Data);
+    }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public ReadOnlyMemory<byte> AsReadOnlyMemory()
+    {
+        return new ReadOnlyMemory<byte>(Data);
+    }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public ReadOnlySpan<byte> AsReadOnlySpan()
+    {
+        return new ReadOnlySpan<byte>(Data);
+    }
+
+    /// <inheritdoc cref="IImageMemory" />
+    public Span<byte> AsSpan()
+    {
+        return new Span<byte>(Data);
+    }
+
+    /// <inheritdoc cref="object" />
+    public override string ToString()
+    {
+        var length = Data.Length;
+        var unit = "byte";
+        if (Data.Length > 1024 && Data.Length < 1024 * 1024)
+        {
+            length /= 1024;
+            unit = "kb";
+        }
+        else if (Data.Length > 1024 * 1024 && Data.Length < 1024 * 1024 * 1024)
+        {
+            length /= 1024 * 1024;
+            unit = "mb";
+        }
+        else if (Data.Length > 1024 * 1024 * 1024)
+        {
+            length /= 1024 * 1024 * 1024;
+            unit = "gb";
+        }
+
+        return $"Alignment: {Alignment}, Color Channels: {ColorChannels}, Precision: {Precision}, Size: {length}{unit}";
+    }
+
+    #endregion
 }
