e9989c4f1044259b713df14bcc8baf3fef359ff2
Stefan Loeffler <s.loeffler@custom-co.de>
2022-06-11 18:12:53 +0200

    Upgrade to file scoped namespaces

--- src/Test/Domain/Imaging.Contracts/DimensionTests.cs
+++ src/Test/Domain/Imaging.Contracts/DimensionTests.cs
@@ -1,104 +1,104 @@
-namespace CustomCode.Domain.Imaging.Tests
+namespace CustomCode.Domain.Imaging.Tests;
+
+using Xunit;
+
+public sealed class DimensionTests
 {
-    using Xunit;
-    public sealed class DimensionTests
+    [Fact(DisplayName = "Dimension from constant width and height")]
+    public void CreateDimensionFromConstantWidthAndHeight()
     {
-        [Fact(DisplayName = "Dimension from constant width and height")]
-        public void CreateDimensionFromConstantWidthAndHeight()
-        {
-            // Given
+        // Given
 
-            // When
-            var dimension = new Dimension(1u, 2u);
+        // When
+        var dimension = new Dimension(1u, 2u);
 
-            // Then
-            Assert.Equal(1u, dimension.Width);
-            Assert.Equal(2u, dimension.Height);
-        }
+        // Then
+        Assert.Equal(1u, dimension.Width);
+        Assert.Equal(2u, dimension.Height);
+    }
 
-        [Fact(DisplayName = "Dimension from tuple")]
-        public void CreateDimensionFromTuple()
-        {
-            // Given
-            var tuple = (1u, 2u);
-            
-            // When
-            var dimension = new Dimension(tuple);
+    [Fact(DisplayName = "Dimension from tuple")]
+    public void CreateDimensionFromTuple()
+    {
+        // Given
+        var tuple = (1u, 2u);
+        
+        // When
+        var dimension = new Dimension(tuple);
 
-            // Then
-            Assert.Equal(1u, dimension.Width);
-            Assert.Equal(2u, dimension.Height);
-        }
+        // Then
+        Assert.Equal(1u, dimension.Width);
+        Assert.Equal(2u, dimension.Height);
+    }
 
-        [Fact(DisplayName = "Dimension from other dimension")]
-        public void CreateDimensionFromOtherDimension()
-        {
-            // Given
-            var other = new Dimension(1u, 2u);
+    [Fact(DisplayName = "Dimension from other dimension")]
+    public void CreateDimensionFromOtherDimension()
+    {
+        // Given
+        var other = new Dimension(1u, 2u);
 
-            // When
-            var dimension = new Dimension(other);
+        // When
+        var dimension = new Dimension(other);
 
-            // Then
-            Assert.Equal(1u, dimension.Width);
-            Assert.Equal(2u, dimension.Height);
-        }
+        // Then
+        Assert.Equal(1u, dimension.Width);
+        Assert.Equal(2u, dimension.Height);
+    }
 
-        [Fact(DisplayName = "Deconstruct dimension")]
-        public void DeconstructDimension()
-        {
-            // Given
-            var dimension = new Dimension(1u, 2u);
+    [Fact(DisplayName = "Deconstruct dimension")]
+    public void DeconstructDimension()
+    {
+        // Given
+        var dimension = new Dimension(1u, 2u);
 
-            // When
-            var (width, height) = dimension;
+        // When
+        var (width, height) = dimension;
 
-            // Then
-            Assert.Equal(1u, width);
-            Assert.Equal(2u, height);
-        }
+        // Then
+        Assert.Equal(1u, width);
+        Assert.Equal(2u, height);
+    }
 
-        [Fact(DisplayName = "Assign tuple to dimension")]
-        public void AssignTupleToDimension()
-        {
-            // Given
-            Dimension dimension;
-            var tuple = (1u, 2u);
+    [Fact(DisplayName = "Assign tuple to dimension")]
+    public void AssignTupleToDimension()
+    {
+        // Given
+        Dimension dimension;
+        var tuple = (1u, 2u);
 
-            // When
-            dimension = tuple;
+        // When
+        dimension = tuple;
 
-            // Then
-            Assert.Equal(1u, dimension.Width);
-            Assert.Equal(2u, dimension.Height);
-        }
+        // Then
+        Assert.Equal(1u, dimension.Width);
+        Assert.Equal(2u, dimension.Height);
+    }
 
-        [Fact(DisplayName = "Compare two dimensions for equality")]
-        public void CompareTwoDimensionsForEquality()
-        {
-            // Given
-            var left = new Dimension(1u, 2u);
-            var right = new Dimension(3u, 4u);
+    [Fact(DisplayName = "Compare two dimensions for equality")]
+    public void CompareTwoDimensionsForEquality()
+    {
+        // Given
+        var left = new Dimension(1u, 2u);
+        var right = new Dimension(3u, 4u);
 
-            // When
-            var areEqual = left == right;
+        // When
+        var areEqual = left == right;
 
-            // Then
-            Assert.False(areEqual);
-        }
+        // Then
+        Assert.False(areEqual);
+    }
 
-        [Fact(DisplayName = "Compare two dimensions for inequality")]
-        public void CompareTwoDimensionsForInequality()
-        {
-            // Given
-            var left = new Dimension(1u, 2u);
-            var right = new Dimension(3u, 4u);
+    [Fact(DisplayName = "Compare two dimensions for inequality")]
+    public void CompareTwoDimensionsForInequality()
+    {
+        // Given
+        var left = new Dimension(1u, 2u);
+        var right = new Dimension(3u, 4u);
 
-            // When
-            var areInequal = left != right;
+        // When
+        var areInequal = left != right;
 
-            // Then
-            Assert.True(areInequal);
-        }
+        // Then
+        Assert.True(areInequal);
     }
 }
